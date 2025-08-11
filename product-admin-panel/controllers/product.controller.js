18f5cd8de6d7ca694f6a1c282654068e8c202610
Milan318 <milanmandaviya7495@gmail.com>
2025-08-11 16:23:41 +0530

    done

---
+++ product-admin-panel/controllers/product.controller.js
@@ -0,0 +1,63 @@
+
+const Product = require("../models/productSchema")
+
+module.exports.home = (req,res)=>{
+    res.render('index')
+}
+
+module.exports.addProduct = async (req,res)=>{
+    try{
+        await Product.create(req.body)
+        res.redirect(req.get('Referrer') || '/')
+    }
+    catch(err){
+        console.log(err)
+        res.redirect(req.get('Referrer') || '/')
+    }
+}
+
+module.exports.addProductPage =async (req,res)=>{
+    return res.render('pages/addProductPage')
+}
+
+module.exports.viewProductPage =async (req,res)=>{
+    try {
+        let product = await Product.find({})
+        return res.render('pages/viewProductPage',{product})
+    } catch (error) {
+        console.log(error)
+        return res.render('pages/viewProductPage',{product : []})
+    }
+}
+
+module.exports.deleteProduct = async (req,res)=>{
+    try {
+        let {id} = req.params
+        await Product.findByIdAndDelete(id);
+        res.redirect(req.get("Referrer" || "/"))
+    } catch (error) {
+        console.log(error)
+        res.redirect(req.get("Referrer" || "/"))
+    }
+}
+
+module.exports.editProduct = async (req,res)=>{
+    try {
+        let {id} = req.params;
+        let product = await Product.findById(id)
+        res.render('pages/editProductPage',{product})
+    } catch (error) {
+        console.log(error)
+        res.render('pages/editProductPage',{product : []})
+    }
+}
+module.exports.updateProduct = async (req, res) => {
+    try {
+        let { id } = req.params;
+        await Product.findByIdAndUpdate(id, req.body, { new: true });
+        res.redirect('/viewProductPage');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/viewProductPage');
+    }
+};
